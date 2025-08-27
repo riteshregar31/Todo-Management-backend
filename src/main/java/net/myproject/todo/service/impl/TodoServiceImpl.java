36709b63d7b2238bad35e6428009a67d82b323c8
riteshregar31 <riteshregar31@gmail.com>
2025-08-27 19:25:11 +0530

    added ModelMapper library

--- src/main/java/net/myproject/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/net/myproject/todo/service/impl/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import net.myproject.todo.dto.TodoDto;
 import net.myproject.todo.entity.Todo;
 import net.myproject.todo.repository.TodoRepository;
 import net.myproject.todo.service.TodoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,26 +14,19 @@ public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
 
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
        //todoDto to todo jpa entity
-        Todo todo =new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.isCompleted());
+        Todo todo=modelMapper.map(todoDto,Todo.class);
 
         //jpa entity
         Todo savedTodo = todoRepository.save(todo);
 
         //convert saved entity to dto object
-
-        TodoDto savedTodoDto =new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+        TodoDto savedTodoDto=modelMapper.map(savedTodo,TodoDto.class);
 
         return savedTodoDto;
 
