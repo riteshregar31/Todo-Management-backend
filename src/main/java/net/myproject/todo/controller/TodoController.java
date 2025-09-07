a0b550f9d61198d53bdfbc8f620f56b50fd3a7cb
riteshregar31 <riteshregar31@gmail.com>
2025-09-07 16:54:11 +0530

    add spring security for authentication and role based authorization

--- src/main/java/net/myproject/todo/controller/TodoController.java
+++ src/main/java/net/myproject/todo/controller/TodoController.java
@@ -5,11 +5,12 @@ import net.myproject.todo.dto.TodoDto;
 import net.myproject.todo.service.TodoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/todos")
 @AllArgsConstructor
@@ -18,6 +19,7 @@ import java.util.List;
 private TodoService todoService;
 
 //add todo rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
         TodoDto savedTodo =todoService.addTodo(todoDto);
@@ -27,6 +29,7 @@ private TodoService todoService;
 
     //get todo rest api
      @GetMapping("{id}")
+     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public  ResponseEntity<TodoDto> getTodo(@PathVariable("id") Long todoId){
         TodoDto todoDto = todoService.getTodo(todoId);
         return new ResponseEntity<>(todoDto, HttpStatus.OK);
@@ -35,6 +38,7 @@ private TodoService todoService;
 
     //get all todos rest api
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public ResponseEntity<List<TodoDto>>getAllTodos(){
         List<TodoDto>todos=todoService.getAllTodos();
         return  ResponseEntity.ok(todos);
@@ -42,6 +46,7 @@ private TodoService todoService;
 
     //update todo rest api
     @PutMapping("{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto,@PathVariable("id") Long id){
         TodoDto updatedTodo = todoService.updateTodo(todoDto, id);
         return ResponseEntity.ok(updatedTodo);
@@ -49,6 +54,7 @@ private TodoService todoService;
 
     //Build delete Todo rest api
 @DeleteMapping("{id}")
+@PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deleteTodo(@PathVariable("id") Long id){
         todoService.deleteTodo(id);
         return ResponseEntity.ok("todo deleted successfully");
@@ -56,6 +62,7 @@ private TodoService todoService;
 
     //complete todo rest api
     @PatchMapping("{id}/complete")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable("id") Long id){
         TodoDto updatedDto = todoService.completeTodo(id);
         return ResponseEntity.ok(updatedDto);
@@ -63,6 +70,7 @@ private TodoService todoService;
 
     //incomplete todo rest api
     @PatchMapping("{id}/in-complete")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable Long id){
         TodoDto updatedDto=todoService.inCompleteTodo(id);
         return  ResponseEntity.ok(updatedDto);
---
+++ src/main/java/net/myproject/todo/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package net.myproject.todo.repository;
+
+import net.myproject.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+}
